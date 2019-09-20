4c68d9f176c98f57d997810b59a333073ae77fdf
alu0100951615 <36208590+alu0100951615@users.noreply.github.com>
2019-09-20 18:57:43 +0100

    Functional with many files in Vtt and SRT folder

--- Subtitulos.js
+++ Subtitulos.js
@@ -1,15 +1,31 @@
 const fs = require('fs')
+const vttFolder = 'C:/Users/daniel/Desktop/Subtitulos/VttFolder'
 
-let regex = /[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9]\s-->\s[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9]/;
+let regex = /[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9]\s-->\s[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9]/; //not necesary g because we use regex in loop
 let retornado;
 var reloj = "03:20,220 --> 03:22,050"
 var newData = ""
 var ceros = "00:"
-var puntos = /\./g;
+var puntos = /\./g; //g , because is for all the data in 1 iteration
+var ruta = 'VttFolder/'
+var ruta2 = 'SrtFolder/'
+var data = "";
 
-const readFile = function(){
+const readDir = function(){
+fs.readdirSync(vttFolder).forEach(file => {
+    file = ruta.concat(file);
+    data = ""
+    data = readFile(file);
+    data = NativeData(data);
+    data = Parser(data);
+    file = file.replace(ruta,ruta2);
+    fs.writeFileSync(file.replace(".vtt",".srt"),data);
+  });
+}
+
+const readFile = function(file){
     try{
-        const dataBuffer = fs.readFileSync(process.argv[2])
+        const dataBuffer = fs.readFileSync(file)
         const data = dataBuffer.toString()
         return data;
     }catch(e){
@@ -17,7 +33,14 @@ const readFile = function(){
     }
 }
 
+const NativeData = function(data){
+    DataNet = data.replace("WEBVTT","")
+    DataNet = DataNet.replace(puntos,",");
+    return DataNet;
+}
+
 const Parser = function(data){
+    newData = "";
     var str = data.split(/\n/)
     var i;
     for(i = 0; i < str.length; i++){
@@ -32,14 +55,12 @@ const Parser = function(data){
     return newData;  
 }
 
-const NativeData = function(data){
-    DataNet = data.replace("WEBVTT","")
-    DataNet = DataNet.replace(puntos,",");
-    return DataNet;
-}
 
-var used = NativeData(readFile())
-var final = Parser(used);
 
-fs.writeFileSync(process.argv[2].replace(".vtt",".srt"),final)
+readDir();
+
+//var used = NativeData(readFile())
+//var final = Parser(used);
+
+//fs.writeFileSync(process.argv[2].replace(".vtt",".srt"),final)
 //console.log(regex.test(reloj))
